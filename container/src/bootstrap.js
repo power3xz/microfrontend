f506b24e8ee91d431355cc9eaa017da7be93f1b5
devj <power3xz@gmail.com>
2022-11-06 20:16:09 +0900

    파일 제거

--- container/src/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { mount as mountProducts } from "products/ProductsIndex";
-import { mount as mountCart } from "cart/CartShow";
-
-console.log("container");
-mountProducts(document.querySelector("#dev-products-container"));
-mountCart(document.querySelector("#dev-cart-container"));
