31c92fa3f08aba55aabe10245ea202f3814cda06
devj <power3xz@gmail.com>
2022-11-20 20:46:03 +0900

    feat: progressbar 추가

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -5,6 +5,7 @@ import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
@@ -15,7 +16,7 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Suspense fallback={<div>loading...</div>}>
+          <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth" component={AuthLazy} />
               <Route path="/" component={MarketingLazy} />
